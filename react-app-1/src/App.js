520238b7087a9cf369c85c4ce8a6583b262f8c9e
hamzam999 <hamza.m999@gmail.com>
2022-08-16 21:56:18 +0530

    counter-app

--- react-app-1/src/App.js
+++ react-app-1/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      //key: value pairs
+      counter: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        {/* arrow function everytime */}
+        <button
+          onClick={() => {
+            this.setState({
+              counter: this.state.counter + 1,
+            })
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Increment
+        </button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
