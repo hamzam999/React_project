1dc452b09542800afe81cea40a221856fbf479f2
hamzam999 <hamza.m999@gmail.com>
2022-08-24 21:36:27 +0530

    saecr photos

--- react-app-1/src/components/Photos.js
+++ react-app-1/src/components/Photos.js
@@ -6,6 +6,8 @@ class Photos extends React.Component {
     this.state = {
       photos: [],
       loading: false,
+      filteredPhotos:[],
+      searchValue:""
     }
   }
   componentDidMount() {
@@ -16,18 +18,36 @@ class Photos extends React.Component {
       resp.json().then((response) => {
         this.setState({
           photos: response,
+          filteredPhotos: response,
           loading: false,
         })
       })
     )
   }
   render() {
+    const searchItem = (e) => {
+      this.setState({
+        searchValue: e.target.value,
+      })
+      const matchingItems = this.state.photos.filter((photo) => {
+        return photo.title.includes(e.target.value)
+      })
+      this.setState({
+        filteredPhotos: matchingItems,
+      })
+    }
+
     if (this.state.loading) {
       return <h1>Loading...</h1>
     }
     return (
       <>
-        {this.state.photos.map((photo) => {
+        <input
+          placeholder="Search"
+          value={this.state.searchValue}
+          onChange={searchItem}
+        />
+        {this.state.filteredPhotos.map((photo) => {
           return (
             <div key={photo.id}>
               <img src={photo.url} height={100} width={100} />
